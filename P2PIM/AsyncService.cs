f9cc87d72f302ccd7b14abb162307b5b70bda8a7
xinjiguaike <xinjiguaike@163.com>
2014-06-18 13:28:59 +0800

    Create Branch For P2PIM

---
+++ P2PIM/AsyncService.cs
@@ -0,0 +1,185 @@
+﻿using P2PIM.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace P2PIM
+{
+    public class AsyncService: INotifyPropertyChanged
+    {
+        private int port;
+        private IPAddress serverIP;
+        private TcpListener listener;
+        private TcpClient tcpClient;
+        private TcpClient client;
+        private StreamWriter clientWriter;
+
+
+
+        private string _chatContent;
+        public string ChatContent
+        {
+            get { return _chatContent; }
+            set
+            {
+                _chatContent = value;
+                OnPropertyChanged("ChatContent");
+            }
+        }
+
+        private string _messageToSend;
+        public string MessageToSend
+        {
+            get { return _messageToSend; }
+            set
+            {
+                _messageToSend = value;
+                OnPropertyChanged("MessageToSend");
+            }
+        }
+
+        public AsyncService()
+        {
+            port = 5000;
+        }
+
+        public async Task StartListenAsync()
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            try
+            {
+                tcpClient = await listener.AcceptTcpClientAsync();
+                await ShowChatMessageAsync();
+            }
+            catch(Exception e)
+            {
+                Trace.TraceError("Rudy Trace => Listen Exception: {0}", e.Message);
+            }
+            
+        }
+
+        public async Task StartConnectAsync()
+        {
+            client = new TcpClient();
+            await client.ConnectAsync(IPAddress.Parse(Settings.Default.TargetIP), port);
+            NetworkStream networkStream = client.GetStream();
+            clientWriter = new StreamWriter(networkStream);
+            clientWriter.AutoFlush = true;
+        }
+
+        public void StopListen()
+        {
+            listener.Stop();
+        }
+
+        public void StopConnect()
+        {
+            if (client != null)
+            {
+                if (client.Connected)
+                    client.Close();
+            }
+            if (tcpClient != null)
+            {
+                if (tcpClient.Connected)
+                    tcpClient.Close();
+            }
+        }
+
+        public async Task ShowChatMessageAsync()
+        {
+            string clientEndPoint = tcpClient.Client.RemoteEndPoint.ToString();
+             
+            try
+            {
+                NetworkStream networkStream = tcpClient.GetStream();
+                StreamReader reader = new StreamReader(networkStream);
+                while (true)
+                {
+                    string request = await reader.ReadLineAsync();
+                    if (request != null)
+                    {
+                        Trace.TraceInformation("Rudy Trace => Received service request: " + request);
+                        ChatContent += request + "--[" + clientEndPoint + "]\n";
+                        Trace.TraceInformation("Rudy Trace => Computed response is: " + ChatContent);
+                    }
+                    else
+                    {
+                        Trace.TraceInformation("Rudy Trace => Client closed the connection.");
+                        break; 
+                    }
+                }
+                
+                tcpClient.Close();
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("Rudy Trace => ShowChatMessage Exception: {0}", e.Message);
+                if (tcpClient.Connected)
+                    tcpClient.Close();
+            }
+
+        }
+
+
+        public async Task SendMessageAsync()
+        {
+            await StartConnectAsync();
+            try
+            {
+                Trace.TraceInformation("Rudy Trace => SendMessageAsync Message: {0}", MessageToSend);
+                if (MessageToSend == "")
+                {
+                    MessageBox.Show("You can not send the empty message!");
+                    return;
+                }
+
+                await clientWriter.WriteLineAsync(MessageToSend);
+                MessageToSend = "";
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("Rudy Trace => SendMessageAsync Exception: {0}", e.Message);
+                if (client.Connected)
+                    client.Close();
+            } 
+        }
+
+        
+
+        public async Task SetServerIpAsync()
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync(hostName);
+
+            foreach (IPAddress ip in ipHostInfo.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    serverIP = ip;
+                    break;
+                }
+            }
+            throw new Exception("No IPv4 address for server");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string PropertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+    }
+}
---
+++ P2PIM/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using P2PIM.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P2PIM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private AsyncService serviceAsync;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            serviceAsync = new AsyncService();
+            this.DataContext = serviceAsync;
+        }
+
+        private async void OnStartListen(object sender, RoutedEventArgs e)
+        {
+            btnStartListen.IsEnabled = false;
+            btnStopListen.IsEnabled = true;
+            lbListening.Visibility = Visibility.Visible;
+            await serviceAsync.StartListenAsync();
+        }
+
+        private async void OnSendMessage(object sender, RoutedEventArgs e)
+        {
+            await serviceAsync.SendMessageAsync();
+        }
+
+        private void OnStopListen(object sender, RoutedEventArgs e)
+        {
+            serviceAsync.StopListen();
+            btnStartListen.IsEnabled = true;
+            btnStopListen.IsEnabled = false;
+            lbListening.Visibility = Visibility.Hidden;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Settings.Default.Save();
+            serviceAsync.StopListen();
+        }
+
+        private async void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if((e.Key == Key.Enter) && tbMessageSend.IsFocused)
+                await serviceAsync.SendMessageAsync();
+        }
+
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            await serviceAsync.StartListenAsync();
+        }
+
+    }
+}
