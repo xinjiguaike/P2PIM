944d74cdbc9d9e23298e31005643d1214e1e563c
Rudy Xiong <xinjiguaike@163.com>
2014-07-18 15:36:26 +0800

    refactor server

---
+++ P2PIM_Server/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PIM_Server
+{
+    public class User
+    {
+        public string UserName;
+        public string LocalIpEndPoint;
+    
+        public User(string name, string endpoint)
+        {
+            UserName = name;
+            LocalIpEndPoint = endpoint;
+        }
+    }
+}
